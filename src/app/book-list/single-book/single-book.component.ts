839b8a2be4821fb18247c395e75ace92ee09f1aa
Marc-Antoine Ferland <maf.ferland@gmail.com>
2019-02-25 09:22:44 -0500

    Fix ngOnInit

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Subscription } from 'rxjs';
 import { Book } from "../../models/Book.model";
-import { ActivatedRoute, ParamMap, Router } from "@angular/router";
 import { BooksService } from "../../services/books.service";
-import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: 'app-single-book',
@@ -13,29 +13,61 @@ export class SingleBookComponent implements OnInit {
 
   book: Book;
   books: Book[];
+  subscription: Subscription;
 
   constructor(private route: ActivatedRoute,
-              private booksService: BooksService,
-              private router: Router) {
+    private booksService: BooksService,
+    private router: Router) {
     this.books = this.booksService.books;
   }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-
-    if(this.books.indexOf(this.books[id]) == -1){
-      this.router.navigate(['/books']);
-    } else{
-      this.book = new Book("", "");
-      this.booksService.getSingleBook(+id).then(
-          (book: Book)=>{
-            this.book = book;
-          }
-      );
-    }
+    // this.route.params : Observable<Params>
+    // Puisque c'est un observable, je peux subscribe dessus.
+    // Pour haque mise à jour de `Params` la fonction que je lui donne sera appelé
+    this.subscription = this.route.params.subscribe(this.myCustonOnUpdateFunction);
   }
 
-  onBack(){
+  private myCustonOnUpdateFunction(params: Params) {
+    const id = params.id;
+    // Ici, j'ai modifié ton code
+    // Tu allais chercher l'index d'un objet en ayant déjà récupéré l'objet
+    // En effet, `books[id]` te donnais l'objet (ou `undefined` si manquant)
+    const book = this.books[id];
+
+    // Ici, je joue avec la définition de `Falsy` en JS
+    // Grosso modo, undefined est considéré comme `Faux`
+    // Donc, !undefined => true
+    // https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+    if (!book) {
+      // Si le livre n'existe pas
+      this.router.navigate(['/books']);
+      // En utilisant un `early` return ici
+      // Je m'assure que le reste du code ne soit pas éxécuté
+      // C'est très rare que j'écris if() {} else {}
+      // D'avoir le return ici permet d'assuré le même fonctionnement en réduisant l'indentation
+      // C'est généralement positif de limité l'indentation de ton code :)
+      return;
+    }
+
+    // Je ne sais pas si book contient vraiment l'objet du livre
+    // Donc j'extrapole un peu sur ton code, mais normalement,
+    // Tu as déjà récupéré le livre plus haut.
+    // Si ce n'est pas le cas, tu peux simplement remplacer par la methode loadBook(id)
+    this.book = book;
+
+    // this.loadBook(id);
+  }
+
+  private loadBook(id) {
+    this.booksService.getSingleBook(+id).then(
+      (book: Book) => {
+        this.book = book;
+      }
+    );
+  }
+
+  onBack() {
     this.router.navigate(['/books']);
   }
 
